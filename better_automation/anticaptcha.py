8d933b3ed5d190b3a3a4c4722debece9c65a81a1
Alen <alen.kimov@gmail.com>
2023-08-31 13:52:45 +0600

    v0.5.0
    - hCaptcha
    - Изменил способ создания тасков с прокси

--- better_automation/anticaptcha.py
+++ better_automation/anticaptcha.py
@@ -15,14 +15,14 @@ class AnticaptchaClient:
         self.api_key = api_key
         self.is_invisible = 0
 
-    async def request(self, method: str, payload: dict = None):
+    async def _request(self, method: str, payload: dict = None):
         url = f"{self.BASE_URL}/{method}"
         payload = payload.copy() if payload else {}
         payload.update({"clientKey": self.api_key})
         return await self._session.request("POST", url, json=payload)
 
     @staticmethod
-    async def handle_response(response: aiohttp.ClientResponse) -> dict:
+    async def _handle_response(response: aiohttp.ClientResponse) -> dict:
         data = await response.json()
         if data == 0:
             raise AnticaptchaError
@@ -31,46 +31,39 @@ class AnticaptchaClient:
         return data
 
     async def request_balance(self):
-        response = await self.request("getBalance")
-        data = await self.handle_response(response)
+        response = await self._request("getBalance")
+        data = await self._handle_response(response)
         return data["balance"]
 
-    async def create_task(
+    async def _create_task(
             self,
             task_data: dict,
             *,
+            proxy: str = None,
+            useragent: str = None,
             cookies: str = None,
     ) -> int:
+        if proxy:
+            parsed_url = urlparse(proxy)
+            proxy_type = parsed_url.scheme
+            proxy_address = parsed_url.hostname
+            proxy_port = parsed_url.port
+            proxy_login = parsed_url.username
+            proxy_password = parsed_url.password
+            task_data.update({
+                "proxyType": proxy_type,
+                "proxyAddress": proxy_address,
+                "proxyPort": proxy_port,
+                "proxyLogin": proxy_login,
+                "proxyPassword": proxy_password,
+            })
+        if useragent: task_data.update({"userAgent": useragent})
         if cookies: task_data.update({"cookies": cookies})
-        response = await self.request("createTask", {"task": task_data})
-        data = await self.handle_response(response)
+        response = await self._request("createTask", {"task": task_data})
+        data = await self._handle_response(response)
         return data["taskId"]
 
-    async def create_task_with_proxy(
-            self,
-            task_data: dict,
-            proxy: str,
-            useragent: str,
-            *,
-            cookies: str = None,
-    ) -> int:
-        parsed_url = urlparse(proxy)
-        proxy_type = parsed_url.scheme
-        proxy_address = parsed_url.hostname
-        proxy_port = parsed_url.port
-        proxy_login = parsed_url.username
-        proxy_password = parsed_url.password
-        task_data.update({
-            "proxyType": proxy_type,
-            "proxyAddress": proxy_address,
-            "proxyPort": proxy_port,
-            "proxyLogin": proxy_login,
-            "proxyPassword": proxy_password,
-        })
-        task_data.update({"userAgent": useragent})
-        return await self.create_task(task_data, cookies=cookies)
-
-    async def wait_for_result(
+    async def _wait_for_result(
             self,
             task_id,
             max_seconds: int = 300,
@@ -81,22 +74,24 @@ class AnticaptchaClient:
             raise AnticaptchaError(f"(task_id={task_id}) Task solution expired")
 
         await asyncio.sleep(delay)
-        response = await self.request("getTaskResult", {"taskId": task_id})
-        data = await self.handle_response(response)
+        response = await self._request("getTaskResult", {"taskId": task_id})
+        data = await self._handle_response(response)
         match data["status"]:
             case "processing":
-                return await self.wait_for_result(task_id, max_seconds, current_second + delay, delay)
+                return await self._wait_for_result(task_id, max_seconds, current_second + delay, delay)
             case "ready":
                 return data
 
-    async def report_incorrect_image_captcha(self, task_id):
-        return await self.request("reportIncorrectImageCaptcha", {"taskId": task_id})
-
-    async def report_incorrect_recaptcha(self, task_id):
-        return await self.request("reportIncorrectRecaptcha", {"taskId": task_id})
-
-    async def report_correct_recaptcha(self, task_id):
-        return await self.request("reportCorrectRecaptcha", {"taskId": task_id})
+    async def _recaptcha_task(self, task_type, url: str, site_key: str, task_data: dict = None, **kwargs) -> str:
+        task_data = task_data.copy() if task_data else {}
+        task_data.update({
+            "type": task_type,
+            "websiteURL": url,
+            "websiteKey": site_key,
+        })
+        task_id = await self._create_task(task_data, **kwargs)
+        data = await self._wait_for_result(task_id)
+        return data["solution"]["gRecaptchaResponse"]
 
     async def recaptcha_v2(
             self,
@@ -107,37 +102,31 @@ class AnticaptchaClient:
             recaptcha_data_s: str = None,
             **kwargs,
     ) -> str:
+        task_type = "RecaptchaV2Task" if "proxy" in kwargs else "RecaptchaV2TaskProxyless"
         task_data = {
-            "type": "RecaptchaV2TaskProxyless",
-            "websiteURL": url,
-            "websiteKey": site_key,
             "isInvisible": is_invisible,
         }
         if recaptcha_data_s:
             task_data["recaptchaDataSValue"] = recaptcha_data_s
-        task_id = await self.create_task(task_data, **kwargs)
-        data = await self.wait_for_result(task_id)
-        return data["solution"]["gRecaptchaResponse"]
+        return await self._recaptcha_task(task_type, url, site_key, task_data, **kwargs)
 
-    async def recaptcha_v2_with_proxy(
+    async def hcaptcha(
             self,
             url: str,
             site_key: str,
-            *,
-            proxy: str,
-            useragent: str,
-            is_invisible: bool = False,
-            recaptcha_data_s: str = None,
             **kwargs,
     ) -> str:
-        task_data = {
-            "type": "RecaptchaV2Task",
-            "websiteURL": url,
-            "websiteKey": site_key,
-            "isInvisible": is_invisible,
-        }
-        if recaptcha_data_s:
-            task_data["recaptchaDataSValue"] = recaptcha_data_s
-        task_id = await self.create_task_with_proxy(task_data, proxy, useragent, **kwargs)
-        data = await self.wait_for_result(task_id)
-        return data["solution"]["gRecaptchaResponse"]
+        task_type = "HCaptchaTask" if "proxy" in kwargs else "HCaptchaTaskProxyless"
+        return await self._recaptcha_task(task_type, url, site_key, **kwargs)
+
+    async def _report_incorrect_captcha(self, task_id, report_method: str):
+        return await self._request(report_method, {"taskId": task_id})
+
+    async def report_incorrect_image_captcha(self, task_id):
+        return await self._report_incorrect_captcha(task_id, "reportIncorrectImageCaptcha")
+
+    async def report_incorrect_recaptcha(self, task_id):
+        return await self._report_incorrect_captcha(task_id, "reportIncorrectRecaptcha")
+
+    async def report_correct_recaptcha(self, task_id):
+        return await self._report_incorrect_captcha(task_id, "reportCorrectRecaptcha")
