21d5ba66a22408b70ae301dd4dec6c30061b33c7
Alen <alen.kimov@gmail.com>
2024-01-20 14:01:08 +0300

    v2.1.0.b12
    - Лучшая поимка oauth_code

--- better_automation/google/browser.py
+++ better_automation/google/browser.py
@@ -38,11 +38,17 @@ class GooglePlaywrightBrowserContext:
         await page.goto("https://accounts.google.com/ServiceLogin")
         await page.locator('xpath=//*[@id="identifierId"]').fill(self.account.email)
         await page.locator('xpath=//*[@id="identifierNext"]/div/button/div[3]').click()
-        await page.wait_for_timeout(5_000)  # 5 sec. Ждем, пока загрузиться reCAPTCHA
+        await page.wait_for_load_state("networkidle")
         if await page.locator('xpath=//iframe[@title="reCAPTCHA"]').count() > 0:
             raise CaptchaRequired("Обнаружена reCAPTCHA.")
         await page.locator('xpath=//*[@id="password"]/div[1]/div/div[1]/input').fill(self.account.password)
         await page.locator('xpath=//*[@id="passwordNext"]/div/button/span').click()
+
+        # На новых аккаунтах может выскакивать назойливое напоминание
+        try:
+            await page.get_by_text("Not now").click(timeout=5_000)
+        except TimeoutError:
+            pass
 
         # Если в аккаунт с этого IP не входили ранее, то попросит ввести recovery_email
         recovery_email_button = page.locator('div[data-challengeid="5"]')
@@ -65,6 +71,7 @@ class GooglePlaywrightBrowserContext:
 
         await page.wait_for_url(self.MY_ACCOUNT_URL_PATTERN)
         self._logged_in = True
+        await page.close()
 
     async def oauth2(
             self,
@@ -78,7 +85,7 @@ class GooglePlaywrightBrowserContext:
             prompt: PromptType = None,
             include_granted_scopes: bool | str = True,
             enable_granular_consent: bool | str = True,
-    ) -> tuple[str, str]:
+    ) -> tuple[str | None, str | None]:
         """
         Найдите подобную ссылку: https://accounts.google.com/o/oauth2/v2/auth?client_id=...
         Передайте параметры ссылки (после знака вопроса (?)) в метод oauth2
@@ -102,21 +109,31 @@ class GooglePlaywrightBrowserContext:
         if prompt: params["prompt"] = prompt
         oauth_url = str(URL(oauth_url).with_query(params))
         page = await self.new_page()
+
+        oauth_code = None
+        redirect_url = None
+
+        async def request_handler(request: Request):
+            print()
+            nonlocal oauth_code
+            nonlocal redirect_url
+
+            # Поимка oauth_code и redirect_url основана на знании того, что гугл делает такой редирект:
+            # https://developers.google.com/identity/protocols/oauth2/javascript-implicit-flow#redirecting
+            if request.url.startswith(redirect_uri):
+                redirect_url = URL(request.url)
+                oauth_code = redirect_url.query.get(response_type)
+
+        page.on("request", request_handler)
+
         await page.goto(oauth_url)
         # TODO Поведение страницы может отличаться, если значение prompt != "consent"
         await page.locator(f'[data-identifier="{self.account.email}"]').click()
-        await page.get_by_text("Continue").click(no_wait_after=True)
-
-        # Поимка oauth_code и redirect_url основана на знании того, что гугл делает такой редирект:
-        # https://developers.google.com/identity/protocols/oauth2/javascript-implicit-flow#redirecting
-        oauth_code = None
-        redirect_url = None
-        while not oauth_code:
-            async with page.expect_event("request") as event_info:
-                request: Request = await event_info.value
-                if request.url.startswith(redirect_uri):
-                    redirect_url = URL(request.url)
-                    oauth_code = redirect_url.query.get(response_type)
-
+        await page.wait_for_load_state("networkidle")
+        try:
+            await page.get_by_text("Continue", exact=True).click(no_wait_after=True, timeout=5_000)
+        except TimeoutError:
+            pass
+        await page.wait_for_load_state("networkidle")
         await page.close()
         return oauth_code, str(redirect_url)
